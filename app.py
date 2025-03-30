d121a03ce86281bc2bfa2bd52149c1e61a216364
JimmyLeung <Jimmy_Leung@m.scnu.edu.cn>
2025-03-30 21:56:17 +0800

    update title name

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="智能反诈系统",
+    page_title="「智镜·无垠」",
     layout="wide",
     page_icon="🛡️",
     initial_sidebar_state="expanded"
@@ -15,11 +15,11 @@ st.session_state.neo4j_password = st.secrets['NEO4J_PASSWORD']
 st.logo("logo_name_new.png", size="large", icon_image="logo.png")
 
 start_page = st.Page("start.py", title="欢迎", icon="🎉")
+recognize_page = st.Page('recognize_page.py', title='短信识别', icon='📩')
 bot_page = st.Page('bot.py', title='问答助手', icon='🤖')
+risk_page = st.Page('risk_page.py', title='风险界面', icon='⚠️')
 search_page = st.Page('search.py', title='案件搜索', icon='🔍')
 show_page = st.Page('show_page.py', title='展示界面', icon='🎈')
-risk_page = st.Page('risk_page.py', title='风险界面', icon='⚠️')
-recognize_page = st.Page('recognize_page.py', title='短信识别', icon='📩')
 
 pages = [start_page, bot_page, search_page, show_page, risk_page, recognize_page]
 pg = st.navigation(pages)
--- start.py
+++ start.py
@@ -2,6 +2,7 @@ import streamlit as st
 from streamlit import session_state as ses
 
 
-st.title(":rainbow[Time Series Agent Toolkit]")
+st.title(":rainbow[「智镜·无垠」]")
+st.markdown("### :rainbow[*基于AI与知识图谱的全维度反诈中枢*]")
 video_path = "logo.mp4"
 st.video(video_path, autoplay=True, muted=True, loop=True)
